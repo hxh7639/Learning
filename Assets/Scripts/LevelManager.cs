83aac0c48e0024414bfafdf5bc7e36711e9915f4
Andy <hxh7639@yahoo.com.tw>
2016-07-22 15:54:53 -0500

    GG132 Menu system

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -7,7 +7,11 @@ public class LevelManager : MonoBehaviour {
 	public float autoLoadNextLevelAfter;
 	
 	void Start(){
+		if (autoLoadNextLevelAfter == 0){
+			Debug.Log ("Level auto load disabled");
+		}else {
 		Invoke ("LoadNextLevel", autoLoadNextLevelAfter);
+		}
 	}
 	public void LoadLevel(string name){
 		Debug.Log ("Level load requested for: " + name);
