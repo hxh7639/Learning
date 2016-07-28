04011cfa60583a38ed3d63b7cbc0ad7dbebb8c0f
Andy <hxh7639@yahoo.com.tw>
2016-07-28 13:24:38 -0500

    GG 158 setting up defenders

---
+++ Assets/Scripts/Projectiles.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectiles : MonoBehaviour {
+
+	public float speed, damage;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		transform.Translate (Vector3.right * speed * Time.deltaTime);
+		
+	
+	}
+	
+	
+}
