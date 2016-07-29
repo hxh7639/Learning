49877d7464799722dda94711da58ba60a7eab668
Andy <hxh7639@yahoo.com.tw>
2016-07-28 19:00:28 -0500

    GG 159

--- Assets/Scripts/Defenders.cs
+++ Assets/Scripts/Defenders.cs
@@ -3,17 +3,4 @@ using System.Collections;
 
 public class Defenders : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-	
-	void OnTriggerEnter2D(){
-		//Debug.Log (name + "trigger enter");
-	}
 }
