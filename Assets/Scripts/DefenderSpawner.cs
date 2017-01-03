a23ed514abce9a46063b2707e4f4f6846b1dd0a1
AcerTouch\Andy <AcerLaptop>
2017-01-03 14:54:58 -0600

    gg 164-2

--- Assets/Scripts/DefenderSpawner.cs
+++ Assets/Scripts/DefenderSpawner.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class DefenderSpawner : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	public Camera myCamera;
+
+	void OnMouseDown(){
+		print (Input.mousePosition);
+		print (SnapToGrid(CalculateWorldPointOfMouseClick()));
+
 	
-	// Update is called once per frame
-	void Update () {
-		
 	}
+
+	Vector2 SnapToGrid (Vector2 rawWorldPos){
+		float newX = Mathf.RoundToInt(CalculateWorldPointOfMouseClick)
+		float newY
+
+		return new Vector2 (newX, newY);
+	
+	}
+
+
+	Vector2 CalculateWorldPointOfMouseClick(){
+		float mouseX = Input.mousePosition.x;
+		float mouseY = Input.mousePosition.y;
+		float distanceFromCamera = 10f; // not really needed since its 2D game - authographic camera.
+
+		Vector3 weirdTriplet = new Vector3 (mouseX, mouseY, distanceFromCamera); // should work without the Z value
+		Vector2 worldPos = myCamera.ScreenToWorldPoint (weirdTriplet);
+		return worldPos;
+	}
+
 }
