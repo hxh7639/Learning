edf7d76530acdb8d301d5611661ae6bd9da4dee1
AcerTouch\Andy <AcerLaptop>
2017-01-10 14:22:43 -0600

    GG 166-1

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -5,14 +5,29 @@ public class Shooter : MonoBehaviour {
 
 	public GameObject projectile, gun;
 	private GameObject projectileParent;
+	private Animator animator;
 
 	void Start (){
+		animator = GameObject.FindObjectOfType<Animator> (); // On your GameObject it self, find its Animator
 		projectileParent = GameObject.Find ("Projectiles");
 		if (!projectileParent) {
 			projectileParent = new GameObject("Projectiles");
 		}
 	}
-	
+
+	void Update (){
+		if (isAttackerAheadInLane ()) {
+			animator.SetBool ("isAttacking", true);
+		} else {
+			animator.SetBool ("isAttacking", false);
+		}
+	}
+
+	bool isAttackerAheadInLane(){
+		return false;
+	}
+
+
 	private void Fire (){
 		GameObject newProjectile = Instantiate(projectile)as GameObject;
 		newProjectile.transform.parent = projectileParent.transform;
