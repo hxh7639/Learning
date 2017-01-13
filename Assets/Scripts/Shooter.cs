0c59461ba67787da79ad960ba56d621dded5616e
AcerTouch\Andy <AcerLaptop>
2017-01-13 13:41:13 -0600

    GG 166-2

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,8 +4,10 @@ using System.Collections;
 public class Shooter : MonoBehaviour {
 
 	public GameObject projectile, gun;
+
 	private GameObject projectileParent;
 	private Animator animator;
+	private Spawner myLaneSpawner;
 
 	void Start (){
 		animator = GameObject.FindObjectOfType<Animator> (); // On your GameObject it self, find its Animator
@@ -21,6 +23,15 @@ public class Shooter : MonoBehaviour {
 		} else {
 			animator.SetBool ("isAttacking", false);
 		}
+	}
+
+	// look through all spawners, and setmyLaneSpanwer if found
+	void setMylaneSpawner(){
+		Spawner[] spawnerArray = GameObject.FindObjectsOfType<Spawner> (); // find the object with the script?
+		//spawner position is interger
+		//if spawner Y position is 1
+		//set it as Spawner_1
+	
 	}
 
 	bool isAttackerAheadInLane(){
