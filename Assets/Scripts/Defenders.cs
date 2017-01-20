3d9833fe6c7d7a7a3ff8cbfc38caa6e769d16afb
Andy <hxh7639@yahoo.com.tw>
2017-01-19 23:35:29 -0600

    gg 167 end

--- Assets/Scripts/Defenders.cs
+++ Assets/Scripts/Defenders.cs
@@ -3,9 +3,16 @@ using System.Collections;
 
 public class Defenders : MonoBehaviour {
 
+	private StarDisplay starDisplay;
+
+	void Start() {
+		starDisplay = GameObject.FindObjectOfType<StarDisplay>();
+
+	}
 	// Only beingn used as a tag for now!
 	public void AddStar (int amount){
-		print (amount);
+		starDisplay.AddStars(amount);
 	}
+
 
 }
---
+++ Assets/Scripts/StarDisplay.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class StarDisplay : MonoBehaviour {
+
+
+private Text startext;
+private int starAmount = 0;
+	
+ 
+	// Use this for initialization
+	void Start () {
+		startext = GameObject.Find("Stars").GetComponent<Text>(); //course used just getcomponent<text>.. because it is on the same object
+		startext.text = starAmount.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void AddStars(int amount){
+		starAmount += amount;
+		startext.text = starAmount.ToString(); // course factored this out and made it into another function to call here.
+	}
+	public void UseStars(int amount){
+		starAmount -= amount;
+		startext.text = starAmount.ToString();
+	}
+}
