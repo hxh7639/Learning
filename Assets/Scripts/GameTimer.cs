6d62508ef5ded173fa7628d633c6ebff2c4c43d1
Andy <hxh7639@yahoo.com.tw>
2017-01-23 23:07:31 -0600

    GG 170 end

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour {
+
+	public float levelTotalTime = 10;
+
+	private Slider slider;
+	private LevelManager levelManager;
+	private bool levelIsEnded = false;
+	private AudioSource audioSource;
+	private GameObject winLabel;
+	private GameObject loseCollider;
+
+
+	// Use this for initialization
+	void Start () {
+		levelManager = FindObjectOfType<LevelManager>();
+		slider = GetComponent<Slider>();
+		audioSource = GetComponent<AudioSource>();
+		FindYouWinObj ();
+		winLabel.SetActive(false);
+		loseCollider = GameObject.Find("Lose Collider");
+		loseCollider.SetActive(true);
+	}
+
+	void FindYouWinObj ()
+	{
+		winLabel = GameObject.Find ("You Win");
+		if (!winLabel) {
+			Debug.LogWarning ("Please Create 'You Win' object");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		slider.value = Time.timeSinceLevelLoad / levelTotalTime;
+
+		bool levelTimeIsUp = ( slider.value >= 1);
+		if (levelTimeIsUp && levelIsEnded == false){    //course used Time.timeSinceLevelLoad >= LevelSeconds && !isEndOfLevel)
+			audioSource.Play();
+			Invoke ("LoadNextLevel", audioSource.clip.length);
+			levelIsEnded = true;
+			winLabel.SetActive(true);
+			loseCollider.SetActive(false);
+		}
+
+	}
+
+	void LoadNextLevel(){
+		levelManager.LoadNextLevel();
+	}
+
+
+
+}
