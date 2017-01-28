f373363bac1abbb84afa440efd32787a13ef21d7
DESKTOP-O7C4PR2\Uno <hxh7639@yahoo.com.tw>
2017-01-28 15:37:40 -0600

    GG 172 end

---
+++ Assets/Scripts/Scarecrow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scarecrow : MonoBehaviour {
+
+    private Animator anim;
+    private Health targetHealth;
+    public float damage = 10;
+
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+        
+
+    void OnTriggerStay2D(Collider2D collider){
+        GameObject obj = collider.gameObject;
+        if (obj.GetComponent<Attacker>())
+        {
+            targetHealth = obj.GetComponent<Health>();
+            anim.SetTrigger("underAttack Trigger");
+        }
+    }
+
+    void HitCurrentTarget(){
+        targetHealth.HandleHits(damage);
+    }
+}
--- Assets/Scripts/Stone.cs
+++ Assets/Scripts/Stone.cs
@@ -3,5 +3,22 @@ using System.Collections;
 
 public class Stone : MonoBehaviour {
 
+    private Animator anim;
+
+
+    void Start () {
+        anim = GetComponent<Animator>();
+     }
+
+    void OnTriggerStay2D(Collider2D collider){
+        GameObject obj = collider.gameObject;
+
+        // leave the method if not colliding with defender
+        if (obj.GetComponent<Attacker>())
+        {
+            anim.SetTrigger("underAttack Trigger");
+        }
+    }
+
 
 }
